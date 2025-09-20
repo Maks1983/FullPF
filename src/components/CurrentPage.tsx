21da5d5380b2a54956d7ecf5a444721334bd0e5c
Maks1983 <Maks1983@hotmail.com>
2025-09-21 01:07:05 +0200

    Updated CurrentPage.tsx

--- src/components/CurrentPage.tsx
+++ src/components/CurrentPage.tsx
@@ -81,6 +81,14 @@ const CurrentPage = () => {
             />
           </ErrorBoundary>
 
+          {/* Upcoming Payments */}
+          <ErrorBoundary section="Upcoming Payments">
+            <UpcomingPaymentsTimeline
+              payments={headerData.upcomingPayments}
+              limit={5}
+            />
+          </ErrorBoundary>
+
           {/* Smart Suggestions */}
           <ErrorBoundary section="Smart Suggestions">
             <SmartSuggestions
@@ -91,14 +99,6 @@ const CurrentPage = () => {
               daysUntilPaycheck={suggestionsData.daysUntilPaycheck}
               totalAvailable={suggestionsData.totalAvailable}
               monthlyExpenses={suggestionsData.monthlyExpenses}
-            />
-          </ErrorBoundary>
-
-          {/* Upcoming Payments */}
-          <ErrorBoundary section="Upcoming Payments">
-            <UpcomingPaymentsTimeline
-              payments={headerData.upcomingPayments}
-              limit={5}
             />
           </ErrorBoundary>
         </div>
