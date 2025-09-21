e079fe02fd751fcb9f9def614b7dfbb65941fbaf
Maks1983 <Maks1983@hotmail.com>
2025-09-21 11:38:24 +0200

    Fix SmartInsights import in DashboardPage

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,7 @@
+@@ .. @@
+ import DashboardHeader from '../components/dashboard/DashboardHeader';
+ import FinancialHealthScore from '../components/dashboard/FinancialHealthScore';
+ import MetricsGrid from '../components/dashboard/MetricsGrid';
+-import SmartInsights from '../components/dashboard/SmartInsights';
++import { SmartInsights } from '../components/dashboard/SmartInsights';
+ import GoalsSection from '../components/dashboard/GoalsSection';
